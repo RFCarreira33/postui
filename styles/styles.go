43cb8dc00281cfc2c4ad12df1d254a4f3f49afbc
Rodrigo Carreira <rodrigocarreira33@gmail.com>
2023-09-14 21:55:02 +0100

    Initial commit, simple request and ui
    Still getting the hang of Bubletea and everything related to UI development

---
+++ styles/styles.go
@@ -0,0 +1,17 @@
+package styles
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	ColumnStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.HiddenBorder())
+	FocusedStyle = lipgloss.NewStyle().
+			Padding(1, 2).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("202"))
+	HelpStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241"))
+	ItemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	SelectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("202"))
+)
